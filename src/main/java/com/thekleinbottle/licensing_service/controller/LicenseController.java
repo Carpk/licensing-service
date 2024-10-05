d4df744d474499b2afbc5904ef6a959cee9cc144
Invalid <zappeln@gmail.com>
2024-10-04 20:41:53 -0500

    getters and setters

--- src/main/java/com/thekleinbottle/licensing_service/controller/LicenseController.java
+++ src/main/java/com/thekleinbottle/licensing_service/controller/LicenseController.java
@@ -1,5 +1,36 @@
 package com.thekleinbottle.licensing_service.controller;
 
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.thekleinbottle.licensing_service.model.License;
+import com.thekleinbottle.licensing_service.service.LicenseService;
+
+@RestController
+@RequestMapping(value="v1/organization/{organizationId}/license")
 public class LicenseController {
+
+    @Autowired
+    private LicenseService licenseService;
+
+    @GetMapping(value="/{licenseId}")
+    public ResponseEntity<License> getLicense(@PathVariable("organizationId") String organizationId,
+        	@PathVariable("licenseId") String licenseId) {
+
+    	License license = licenseService.getLicense(licenseId,organizationId);
+
+    	return ResponseEntity.ok(license);
+    }
 
 }
--- src/main/java/com/thekleinbottle/licensing_service/service/LicenseService.java
+++ src/main/java/com/thekleinbottle/licensing_service/service/LicenseService.java
@@ -1,5 +1,51 @@
 package com.thekleinbottle.licensing_service.service;
 
+import java.util.Random;
+
+import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+
+import com.thekleinbottle.licensing_service.model.License;
+
+@Service
 public class LicenseService {
 
+  public License getLicense(String licenseId, String organizationId) {
+    License license = new License();
+    license.setId(new Random().nextInt(1000));
+    license.setLicenseId(licenseId);
+    license.setOrganizationId(organizationId);
+    license.setDescription("Software Product");
+    license.setProductName("Ostock");
+    license.setLicenseType("full");
+    
+    return license;
+  }
+
+  public String createLicense(License license, String organizationId) {
+    String responseMessage = null;
+    if (license != null) {
+      license.setOrganizationId(organizationId);
+      responseMessage = String.format("This is the post and the object is: %s", license.toString());
+    }
+
+    return responseMessage;
+  }
+
+  public String updateLicense(License license, String organizationId) {
+    String responseMessage = null;
+    if (license != null) {
+      license.setOrganizationId(organizationId);
+      responseMessage = String.format("This is the put and the object is: %s", license.toString());
+    }
+
+    return responseMessage;
+  }
+
+  public String deleteLicense(License licenseId, String organizationId) {
+    String responseMessage = null;
+    responseMessage = String.format("Deleting license with id %s for the organization %s", licenseId, organizationId);
+
+    return responseMessage;
+  }
 }
