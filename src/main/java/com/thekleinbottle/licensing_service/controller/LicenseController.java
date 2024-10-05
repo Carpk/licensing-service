4c0178d74129daf590d65492b1296343134a403c
Invalid <zappeln@gmail.com>
2024-10-05 14:22:33 -0500

    working https requests

--- src/main/java/com/thekleinbottle/licensing_service/controller/LicenseController.java
+++ src/main/java/com/thekleinbottle/licensing_service/controller/LicenseController.java
@@ -33,4 +33,25 @@ public class LicenseController {
     	return ResponseEntity.ok(license);
     }
 
+    @PutMapping
+    public ResponseEntity<String> updateLicense(@PathVariable("organizationId") String organizationId, 
+            @RequestBody License request) {
+        
+        return ResponseEntity.ok(licenseService.updateLicense(request, organizationId));
+    }
+
+    @PostMapping
+    public ResponseEntity<String> createLicense(@PathVariable("organizationId") String organizationId,
+            @RequestBody License request) {
+        
+        return ResponseEntity.ok(licenseService.createLicense(request, organizationId));
+    }
+
+    @DeleteMapping(value="/{licenseId}")
+    public ResponseEntity<String> deleteLicense(@PathVariable("organizationId") String organizationId,
+            @PathVariable("licenseId") String licenseId) {
+
+        return ResponseEntity.ok(licenseService.deleteLicense(licenseId, organizationId));
+    }
+     
 }
